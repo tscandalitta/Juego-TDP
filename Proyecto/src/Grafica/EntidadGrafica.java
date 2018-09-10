75c342a6e69b232b8ee69a7844a2095d1f4ede63
Tomas <tscandalitta@gmail.com>
2018-09-10 17:55:20 -0300

    no message

--- Proyecto/src/Grafica/EntidadGrafica.java
+++ Proyecto/src/Grafica/EntidadGrafica.java
@@ -1,5 +1,13 @@
 package Grafica;
 
-public class EntidadGrafica {
+import javax.swing.Icon;
+import javax.swing.JLabel;
 
+public class EntidadGrafica {
+	protected JLabel grafico;
+	protected Icon image[];
+	protected int width;
+	protected int height;
+	
+	
 }
