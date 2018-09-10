88de931a4082004950950397782de1872ae72959
Tomas <tscandalitta@gmail.com>
2018-09-10 17:32:09 -0300

    no message

--- Proyecto/src/Personajes/Entidad.java
+++ Proyecto/src/Personajes/Entidad.java
@@ -1,5 +1,32 @@
 package Personajes;
 
-public abstract class Entidad {
+import Grafica.EntidadGrafica;
 
+public abstract class Entidad {
+	protected int vida;
+	protected int x, y;
+	
+	protected EntidadGrafica grafico;
+	
+	protected Entidad(int x, int y) {
+		this.x= x;
+		this.y= y;
+		this.grafico = null;
+	}
+	
+	public int getVida() {
+		return vida;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	protected void mover(int dir){
+		
+	}
 }
