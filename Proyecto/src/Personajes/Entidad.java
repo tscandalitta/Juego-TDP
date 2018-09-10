4d00e477d47fc61e6f753b2766ab9b3f126f869c
LAPTOP-K0VP9IR8\Horus <horusmoro2015@gmail.com>
2018-09-10 17:07:15 -0300

    Prototipo de clase jugador ,entidad y mapa

--- Proyecto/src/Personajes/Entidad.java
+++ Proyecto/src/Personajes/Entidad.java
@@ -1,5 +1,14 @@
 package Personajes;
 
 public abstract class Entidad {
-
+	protected int vida;
+	protected int posX;
+	protected int posY;
+	
+	protected Entidad() {
+		vida=0;
+		posX=0;
+		posY=0;
+	}
+    protected void colisionar() {}
 }
