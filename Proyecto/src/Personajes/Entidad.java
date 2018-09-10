bb817d5ad77c3a9260b01cbe2d0f0ad1cab5d011
LAPTOP-K0VP9IR8\Horus <horusmoro2015@gmail.com>
2018-09-10 18:00:39 -0300

    actualizacion clases

--- Proyecto/src/Personajes/Entidad.java
+++ Proyecto/src/Personajes/Entidad.java
@@ -4,11 +4,11 @@ public abstract class Entidad {
 	protected int vida;
 	protected int posX;
 	protected int posY;
-	
-	protected Entidad() {
-		vida=0;
-		posX=0;
-		posY=0;
+	protected Entidad(int v,int x, int y) {
+		vida=v;
+		posX=x;
+		posY=y;
 	}
-    protected void colisionar() {}
+
+	protected void colisionar() {}
 }
--- Proyecto/src/Personajes/Jugador.java
+++ Proyecto/src/Personajes/Jugador.java
@@ -1,5 +1,24 @@
 package Personajes;
 
-public class Jugador {
+public class Jugador extends Entidad {
+	private Disparador arma;
+	private boolean premioActivado;
+	
+	public Jugador(int x,int y) {
+		super(100,x,y);
+		premioActivado=false;
+	}
+	public void disminuirVida(int n) {
+		vida-=n;
+		if(vida<=0)
+			finJuego();	
+	}
+	public boolean activarPremio() {
+		premioActivado=true;
+	}
+	public boolean desactivarPremio() {
+		premioActivado=false;
+	}
+	
 
 }
