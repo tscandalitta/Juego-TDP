80d9b7e5ffdea278bd6637bc02a6853cd2c3b8ab
Tomas <tscandalitta@gmail.com>
2018-11-10 19:40:47 -0300

    no message

--- Proyecto/src/Inteligencias/InteligenciaKamikazeRandom.java
+++ Proyecto/src/Inteligencias/InteligenciaKamikazeRandom.java
@@ -5,28 +5,24 @@ import Personajes.Enemigo;
 public class InteligenciaKamikazeRandom extends InteligenciaKamikaze{
 	protected int direccionY=1;
 	protected int limiteSuperior, limiteInferior;
-	protected int x,y,t;
+	protected double x,y,t;
 	
 	public InteligenciaKamikazeRandom(Enemigo k) {
 		super(k);
-		//formula para generar un numero aleatorio dentro de un intervalo
-		limiteSuperior = (int) Math.floor(Math.random()*(200-0+1)+0);
-		limiteInferior = (int) Math.floor(Math.random()*(650-500+1)+500);
-		x=1280;
+		t=610;
 	}
 	
 	protected void moverAux() {
-	/**	x-=0.1;
-		y=(int) (150*Math.sin(-x/20))+360;
+		t-=3;
+		x=2*t;
+		y= this.sinModified(t);
 		pos.setLocation(x, y);
-		System.out.println(x+","+y);
-		*/
-		pos.setLocation(pos.x -velocidad, pos.y - direccionY*velocidad);
-		if(pos.y<limiteSuperior)
-			direccionY=-direccionY;
-		if(pos.y>limiteInferior)
-			direccionY=-direccionY;
-		
+		if(pos.x<-10)
+			t=610;
+	}
+	
+	private double sinModified(double x) {
+		return (175*Math.sin(-x/50))+360;
 	}
 
 }
